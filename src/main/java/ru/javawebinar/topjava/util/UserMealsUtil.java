57e8d72681b2b2f8fc35039fa806e3bd1b01383b
Kostennik <kostennik@gmail.com>
2020-04-25 14:50:12 +0200

    complete loop realization

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -21,31 +21,32 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        final List<UserMealWithExceed> filtered = getFilteredWithExceeded(mealList, LocalTime.of(10, 0), LocalTime.of(21, 0), 2000);
+        final List<UserMealWithExceed> filtered = getFilteredWithExceeded(mealList, LocalTime.of(10, 0), LocalTime.of(13, 0), 2000);
         filtered.forEach(System.out::println);
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExceed> exceeds = new ArrayList<>();
         Map<LocalDate, Integer> map = new HashMap<>();
+        List<UserMeal> meals = new ArrayList<>();
         for (UserMeal meal : mealList) {
-
+            if(isBetween(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                meals.add(meal);
+            }
             final LocalDate date = meal.getDateTime().toLocalDate();
-            boolean isBetween = isBetween(meal.getDateTime().toLocalTime(), startTime, endTime);
             int value = map.getOrDefault(date, 0) + meal.getCalories();
-            boolean iGreater = value > caloriesPerDay;
-
-            addMealWithExceed(exceeds, meal, isBetween, iGreater);
             map.put(date, value);
+        }
+        for(UserMeal meal : meals) {
+            exceeds.add(new UserMealWithExceed(
+                    meal.getDateTime(),
+                    meal.getDescription(),
+                    meal.getCalories(),
+                    map.get(meal.getDateTime().toLocalDate()) > caloriesPerDay));
         }
         return exceeds;
     }
 
-    private static void addMealWithExceed(List<UserMealWithExceed> exceeds, UserMeal meal, boolean isBetween, boolean isGreater) {
-        if (isBetween && isGreater) {
-            exceeds.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true));
-        } else if (isBetween) {
-            exceeds.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false));
-        }
-    }
+
+
 }
