26b1b6ff51c13253f61f6b31540b177d3721684e
Kostennik <kostennik@gmail.com>
2020-05-24 11:10:09 +0200

    add some more color into report

--- src/test/java/ru/javawebinar/topjava/TimeWatcher.java
+++ src/test/java/ru/javawebinar/topjava/TimeWatcher.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 
 public class TimeWatcher extends TestWatcher {
     private static final Logger log = LoggerFactory.getLogger(TimeWatcher.class);
+    private static final String RED_MARK = "\u001B[31m" + "x" + "\u001B[30m";
+    private static final String GREEN_MARK = "\u001B[32m" + "v" + "\u001B[30m";
     private long startTime;
     private static long totalTime;
     private static StringBuilder report = new StringBuilder();
@@ -19,31 +21,41 @@ public class TimeWatcher extends TestWatcher {
     }
 
     @Override
-    protected void finished(Description description) {
-        super.finished(description);
-        long duration = System.currentTimeMillis() - startTime;
-        String methodName = description.getMethodName();
-        log.info("Test {} time: {} ms.", methodName, duration);
-        totalTime += duration;
-        report.append(String.format("\n | %-30s | %8s |", methodName, duration));
-        startTime = 0L;
+    protected void succeeded(Description description) {
+        super.succeeded(description);
+        setResult(description, GREEN_MARK);
+    }
+
+    @Override
+    protected void failed(Throwable e, Description description) {
+        super.failed(e, description);
+        setResult(description, RED_MARK);
     }
 
     public static void initReport(String className) {
         report = new StringBuilder();
         totalTime = 0L;
         String[] arr = className.split("\\.");
-        report.append("\n" + "______________________________________________");
-        report.append(String.format("\n %-10s %s", "", arr[arr.length - 1]));
-        report.append("\n" + "______________________________________________");
-        report.append(String.format("\n | %-30s | %8s |", "TEST NAME", "TIME(ms)"));
-        report.append("\n" + "______________________________________________");
+        report.append("\n" + "__________________________________________________");
+        report.append(String.format("\n %-15s %s", "", arr[arr.length - 1]));
+        report.append("\n" + "__________________________________________________");
+        report.append(String.format("\n | %-34s | %8s |", "TEST NAME", "TIME(ms)"));
+        report.append("\n" + "__________________________________________________");
     }
 
     public static String getReport() {
-        report.append("\n" + "______________________________________________");
-        report.append(String.format("\n | %-30s | %8s |", "TOTAL", totalTime));
-        report.append("\n" + "______________________________________________");
+        report.append("\n" + "__________________________________________________");
+        report.append(String.format("\n | %-34s | %8s |", "TOTAL", totalTime));
+        report.append("\n" + "__________________________________________________");
         return report.toString();
+    }
+
+    private void setResult(Description description, String redMark) {
+        long duration = System.currentTimeMillis() - startTime;
+        String methodName = description.getMethodName();
+        log.info("Test {} time: {} ms.", methodName, duration);
+        totalTime += duration;
+        report.append(String.format("\n | %s | %-30s | %8s |", redMark, methodName, duration));
+        startTime = 0L;
     }
 }
