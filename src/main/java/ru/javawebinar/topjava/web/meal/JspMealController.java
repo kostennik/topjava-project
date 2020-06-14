8cc98a82d37d77fe6351aa6b2d096f56a6f824fa
Kostennik <kostennik@gmail.com>
2020-06-14 18:42:05 +0200

    add mapping delete/update/mealForm

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -6,18 +6,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import static ru.javawebinar.topjava.web.meal.JspMealController.PATH;
+
 @Controller
+@RequestMapping(PATH)
 public class JspMealController {
     private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
+    public static final String PATH = "/meals";
+    public static final String MEAL_FORM_PATH = "/mealForm";
+    public static final String UPDATE_PATH = "/update";
+    public static final String DELETE_PATH = "/delete";
 
     private final MealService mealService;
 
@@ -25,7 +34,7 @@ public class JspMealController {
         this.mealService = mealService;
     }
 
-    @GetMapping("/meals")
+    @GetMapping
     public String getAll(Model model) {
         int userId = SecurityUtil.authUserId();
         log.info("getAll for user {}", userId);
@@ -33,7 +42,7 @@ public class JspMealController {
         return "meals";
     }
 
-    @GetMapping("/mealForm")
+    @GetMapping(UPDATE_PATH)
     public String get(HttpServletRequest request, Model model) {
         int userId = SecurityUtil.authUserId();
         int id = getId(request);
@@ -42,27 +51,20 @@ public class JspMealController {
         return "mealForm";
     }
 
-    @PostMapping("/get")
-    public String update(HttpServletRequest request, Model model) {
-        int userId = SecurityUtil.authUserId();
-        int id = getId(request);
-        log.info("get meal {} for user {}", id, userId);
-        model.addAttribute("id", id);
-        model.addAttribute("meal", mealService.get(id, userId));
-        return "redirect:mealForm";
-    }
-
-    @PostMapping("/delete")
+    @GetMapping(DELETE_PATH)
     public String delete(HttpServletRequest request) {
         int userId = SecurityUtil.authUserId();
         int id = getId(request);
         log.info("delete meal {} for user {}", id, userId);
         mealService.delete(id, userId);
-        return "redirect:meals";
+        final StringBuffer requestURL = request.getRequestURL();
+        requestURL.setLength(requestURL.length() - DELETE_PATH.length());
+        return "redirect:" + requestURL.toString();
     }
 
-    @PostMapping("/update")
-    public String update(HttpServletRequest request) {
+    @PostMapping(MEAL_FORM_PATH)
+    public String update(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
         int userId = SecurityUtil.authUserId();
         int id = getId(request);
         Meal meal = new Meal(
@@ -72,7 +74,9 @@ public class JspMealController {
                 Integer.parseInt(request.getParameter("calories")));
         log.info("update {} for user {}", meal, userId);
         mealService.update(meal, userId);
-        return "meals";
+        final StringBuffer requestURL = request.getRequestURL();
+        requestURL.setLength(requestURL.length() - MEAL_FORM_PATH.length());
+        return "redirect:" + requestURL.toString();
     }
 
     private int getId(HttpServletRequest request) {
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -49,21 +49,12 @@
             <jsp:useBean id="meal" type="ru.javawebinar.topjava.to.MealTo"/>
             <tr data-mealExcess="${meal.excess}">
                 <td>
-                        <%--${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}--%>
-                        <%--<%=TimeUtil.toString(meal.getDateTime())%>--%>
-                        <%--${fn:replace(meal.dateTime, 'T', ' ')}--%>
                         ${fn:formatDateTime(meal.dateTime)}
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><form method="post" action="get">
-                    <input type="id" name="id" value="${meal.id}" hidden>
-                    <input type="submit" value="Update">
-                </form></td>
-                <td><form name="delete" method="post" action="delete">
-                    <input type="id" name="id" value="${meal.id}" hidden>
-                    <input type="submit" value="Delete">
-                </form></td>
+                <td><a href="meals/update?id=${meal.id}">Update</a></td>
+                <td><a href="meals/delete?id=${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
