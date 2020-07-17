a78a1996dd7239596a56f1900aef90aca5f4af6a
Kostennik <kostennik@gmail.com>
2020-07-17 20:29:07 +0200

    make AbstractRestController

--- src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
@@ -2,48 +2,38 @@ package ru.javawebinar.topjava.web.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
-import ru.javawebinar.topjava.web.AbstractControllerTest;
-import ru.javawebinar.topjava.web.json.JsonUtil;
+import ru.javawebinar.topjava.web.AbstractRestController;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.TestData.assertMatch;
 import static ru.javawebinar.topjava.UserTestData.*;
 import static ru.javawebinar.topjava.web.user.ProfileRestController.REST_URL;
 
-class ProfileRestControllerTest extends AbstractControllerTest {
+class ProfileRestControllerTest extends AbstractRestController<User> {
 
     @Autowired
     private UserService userService;
 
+    public ProfileRestControllerTest() {
+        super(REST_URL, User.class, IGNORE_FIELDS);
+    }
+
     @Test
     void get() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(contentJson(USER));
+        super.get("", USER);
     }
 
     @Test
     void delete() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete(REST_URL))
-                .andExpect(status().isNoContent());
-        assertMatch(userService.getAll(), ADMIN);
+        super.delete("", () -> userService.get(USER_ID));
     }
 
     @Test
     void update() throws Exception {
         User updated = UserTestData.getUpdated();
-        mockMvc.perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(updated)))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-
-        assertMatch(userService.get(USER_ID), updated);
+        super.update("", updated);
+        assertMatch(IGNORE_FIELDS, userService.get(USER_ID), updated);
     }
 }
