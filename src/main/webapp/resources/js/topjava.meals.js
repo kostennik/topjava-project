79c6d92065d8a406544bbcb116203c23f9db5c01
Kostennik <kostennik@gmail.com>
2020-07-25 14:48:06 +0200

    HW8

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,58 +1,43 @@
-$(function () {
-    makeEditable({
-            ajaxUrl: "ajax/meals/",
-            datatableApi: $("#datatable").DataTable({
-                "paging": false,
-                "info": true,
-                "columns": [
-                    {
-                        "data": "dateTime"
-                    },
-                    {
-                        "data": "description"
-                    },
-                    {
-                        "data": "calories"
-                    },
-                    {
-                        "defaultContent": "Edit",
-                        "orderable": false
-                    },
-                    {
-                        "defaultContent": "Delete",
-                        "orderable": false
-                    }
-                ],
-                "order": [
-                    [
-                        0,
-                        "desc"
-                    ]
-                ]
-            })
-        }
-    );
-});
-
-function setDefaultValues() {
-    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 16);
-    $('#dateTime').val(localISOTime);
-    $("#calories").val("1000");
-}
-
-function updateTable() {
-    var formData = $("#filter").serializeArray();
+function updateFilteredTable() {
     $.ajax({
         type: "GET",
-        url: context.ajaxUrl + "filter",
-        data: formData
-    }).done(function (data) {
-        context.datatableApi.clear().rows.add(data).draw();
-    });
+        url: "ajax/profile/meals/filter",
+        data: $("#filter").serialize()
+    }).done(updateTableByData);
 }
 
-function clearFilter() {
-    $("#filter")[0].reset();
-    updateTable();
-}
+$(function () {
+    makeEditable({
+        ajaxUrl: "ajax/profile/meals/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        }),
+        updateTable: updateFilteredTable
+    });
+});
